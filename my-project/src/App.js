8c41a9e3193f24ffa4de00dee4246ee13d54cd9a
Don-tresor05 <dontresor922@gmail.com>
2025-03-28 23:39:18 +0200

    React onclick Listeners

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -81,6 +81,18 @@ export default function App() {
     {rowNumber: 4, rowDescription: 'Charge phone battery', rowAssigned: 'User Four'}
   ]
 
+  const addTodo = () => {
+    if (todos.length > 0) {
+      const newTodo = {
+        rowNumber: todos.length + 1,
+        rowDescription: 'New Todo',
+        rowAssigned: 'User Three'
+      }
+      todos.push(newTodo);
+      console.log(todos);
+    }
+  }
+
   return (
     <div className='mt-5 container'>
       <div className="card">
@@ -89,6 +101,9 @@ export default function App() {
         </div>
         <div className="card-body">
           <TodoTable todos={todos}/>
+          <button className='btn btn-primary' onClick={addTodo}>
+            Add new todo
+          </button>
         </div>
       </div>
       
