e1953fc33b1a5416d8689e97a7c2b8a97e8485ac
Don-tresor05 <dontresor922@gmail.com>
2025-03-28 13:51:01 +0200

    Create a second react component

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -68,7 +68,8 @@
 
 import React from 'react'
 import './App.css';
-import TodoRowItem from './components/TodoRowItem';
+
+import TodoTable from './components/TodoTable';
 
 export default function App() {
 
@@ -86,35 +87,7 @@ export default function App() {
           Your Todo's 
         </div>
         <div className="card-body">
-          <table className='table table-hover'>
-            <thead>
-              <tr>
-                <th scope='col'>#</th>
-                <th scope='col'>Description</th>
-                <th scope='col'>Assigned</th>
-              </tr>
-            </thead>
-            <tbody>
-              <TodoRowItem 
-                   rowNumber={todos[0].rowNumber} 
-                   rowDescription={todos[0].rowDescription} 
-                   rowAssigned={todos[0].rowAssigned}
-              />
-
-             <TodoRowItem 
-                   rowNumber={todos[1].rowNumber} 
-                   rowDescription={todos[1].rowDescription} 
-                   rowAssigned={todos[1].rowAssigned}
-              />
-
-              <TodoRowItem 
-                   rowNumber={todos[2].rowNumber} 
-                   rowDescription={todos[2].rowDescription} 
-                   rowAssigned={todos[2].rowAssigned}
-              />
-              
-            </tbody>
-          </table>
+          <TodoTable todos={todos}/>
         </div>
       </div>
       
